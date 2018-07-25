5e86fd397884a40ff01e3eef6e7284c63408b962
Guilherme Ferreira <360guilherme@gmail.com>
2018-07-25 15:58:10 -0300

    Initial Commit

---
+++ TrocaDeTelas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrocaDeTelas
+//
+//  Created by labmacmini12 on 25/07/2018.
+//  Copyright © 2018 cursoIOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
